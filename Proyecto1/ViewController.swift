09d602ef85aac557db370926eb0dc2735fb51f11
juancalderon <juanestebancalderonovalle@gmail.com>
2024-10-04 15:41:54 -0500

    Initial Commit

---
+++ Proyecto1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proyecto1
+//
+//  Created by Juan Esteban Calderon Ovalle on 4/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Proyecto1Tests/Proyecto1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Proyecto1Tests.swift
+//  Proyecto1Tests
+//
+//  Created by Juan Esteban Calderon Ovalle on 4/10/24.
+//
+
+import Testing
+@testable import Proyecto1
+
+struct Proyecto1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
